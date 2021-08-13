a2e9fdd690244a9c87bac5a24baa0fd5fe098eee
Henrique Moura <88518174+ohenriquemoura@users.noreply.github.com>
2021-08-13 00:23:06 +0000

    ex 1 a 6 feitos com sucesso

--- exe06/main.c
+++ exe06/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+   int l1, l2, l3;
+   scanf("%i %i %i", &l1, &l2, &l3);
+   
+   if(l1>l2+l3||l2>l1+l3||l3>l1+l2){
+     printf("OS LADOS NAO FORMAM UM TRIANGULO");
+   }
+   else{
+     if(l1==l2 && l2==l3){
+       printf("TRIANGULO EQUILATERO");
+       
+     }
+     else if(l1==l2 && l2 !=l3 ||l2==l3 && l3!=l1 || l1==l3 && l1!=l2){
+       printf("TRIANGULO ISOSCELES");
+     }
+     else
+       printf("TRIANGULO ESCALENO");
+
+   }
+
    return 0;
 }
